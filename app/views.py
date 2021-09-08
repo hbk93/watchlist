4402645036c45570702f2658a7003607d4e09d96
Harry Bett <harrybett89@gmail.com>
2021-09-08 23:59:07 +0300

    add search functionality

--- app/views.py
+++ app/views.py
@@ -2,18 +2,25 @@ from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
 
+# Views
 @app.route('/')
 def index():
-
   '''
-  View root page funtion that returns the index page and its data
+  View root page function that returns the index page and its data
   '''
   #Getting popular movie
   popular_movies = get_movies('popular')
   upcoming_movie = get_movies('upcoming')
   now_showing_movie = get_movies('now_playing')
+
   title = 'Home - Welcome to The best Movie Review Website Online'
-  return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
+
+  search_movie = request.args.get('movie_query')
+
+  if search_movie:
+      return redirect(url_for('search',movie_name=search_movie))
+  else:
+      return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 @app.route('/movie/<int:id>')
 def movie(id):
@@ -35,22 +42,3 @@ def search(movie_name):
   title = f'search results for {movie_name}'
   return render_template('search.html',movies = searched_movies)
 
-# Views
-@app.route('/')
-def index():
-  '''
-  View root page function that returns the index page and its data
-  '''
-  #Getting popular movie
-  popular_movies = get_movies('popular')
-  upcoming_movie = get_movies('upcoming')
-  now_showing_movie = get_movies('now_playing')
-
-  title = 'Home - Welcome to The best Movie Review Website Online'
-
-  search_movie = request.args.get('movie_query')
-
-  if search_movie:
-    return redirect(url_for('search',movie_name=search_movie))
-  else:
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
