92bfa5d25fc712239e91c91ab887ff80be60d101
Harry Bett <harrybett89@gmail.com>
2021-09-09 12:30:00 +0300

    import classes Revie and ReviewForm

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
+from .models import review
+from .forms import ReviewForm
+Review = review.Review
 
 # Views
 @app.route('/')
