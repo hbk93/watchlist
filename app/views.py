96217362c9e2d87699e4cf48cc1ebec5d376d144
Harry Bett <harrybett89@gmail.com>
2021-09-09 12:42:52 +0300

    create new_review function

--- app/views.py
+++ app/views.py
@@ -45,3 +45,19 @@ def search(movie_name):
   title = f'search results for {movie_name}'
   return render_template('search.html',movies = searched_movies)
 
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+  form = ReviewForm()
+  movie = get_movie(id)
+
+  if form.validate_on_submit():
+    title = form.title.data
+    review = form.review.data
+    new_review = Review(movie.id,title,movie.poster,review)
+    new_review.save_review()
+    return redirect(url_for('movie',id = movie.id))
+
+  title = f'{movie.title} review'
+  return render_template('new_review.html',title = title, review_form = form,movie=movie)
+  
+
